fd049ba0cb5a61394b5038064fc8d1b219163e6c
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-05-13 09:19:11 -0400

    Created views_all.dart to hold all dart files that contain views

---
+++ lib/views/views_all.dart
@@ -0,0 +1,4 @@
+export 'package:bhs__companion__app/views/schedview.dart';
+export 'package:bhs__companion__app/views/mapview.dart';
+export 'package:bhs__companion__app/views/helpfulview.dart';
+export 'package:bhs__companion__app/views/qrscannerview.dart';
