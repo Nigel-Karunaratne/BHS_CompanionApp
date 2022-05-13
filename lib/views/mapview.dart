468426542560ea3c1994c438e31054ebc086c81e
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-05-13 09:58:26 -0400

    Updated files after reinstalling Flutter SDK (now version 3.0.0)

--- lib/views/mapview.dart
+++ lib/views/mapview.dart
@@ -1,4 +1,6 @@
+//! Only here for development purposes: remove when buildng release app
 // ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 
 class MapView extends StatefulWidget {
@@ -17,6 +19,23 @@ class _MapViewState extends State<MapView> {
       ),
       body: Container(
         
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[900],
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on, color: Colors.grey[50],),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner, color: Colors.grey[50],),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help_center, color: Colors.grey[50],),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.schedule, color: Colors.grey[50],),
+          ),
+        ],
       ),
     );
   }
