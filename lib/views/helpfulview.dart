39328cbc82bb6190046c74cb169040bd10a9b97f
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-05-13 09:19:29 -0400

    Created file

---
+++ lib/views/helpfulview.dart
@@ -0,0 +1,25 @@
+//! Only here for development purposes: remove when buildng release app
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
+import 'package:flutter/material.dart';
+
+class HelpfulLinksView extends StatefulWidget {
+  const HelpfulLinksView({ Key? key }) : super(key: key);
+
+  @override
+  State<HelpfulLinksView> createState() => _HelpfulLinksState();
+}
+
+class _HelpfulLinksState extends State<HelpfulLinksView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Schedule Page"),
+      ),
+      body: Container(
+        
+      ),
+    );
+  }
+}
---
+++ lib/views/qrscannerview.dart
@@ -0,0 +1,25 @@
+//! Only here for development purposes: remove when buildng release app
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
+import 'package:flutter/material.dart';
+
+class QRScannerView extends StatefulWidget {
+  const QRScannerView({ Key? key }) : super(key: key);
+
+  @override
+  State<QRScannerView> createState() => _QRScannerViewState();
+}
+
+class _QRScannerViewState extends State<QRScannerView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Schedule Page"),
+      ),
+      body: Container(
+        
+      ),
+    );
+  }
+}
