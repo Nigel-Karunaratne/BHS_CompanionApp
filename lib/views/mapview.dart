bab8c739d77dd460b4c96b4a80c700c409a3be54
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-05-23 18:35:32 -0400

    Abstracted bottom nav bar into its own class

--- lib/views/mapview.dart
+++ lib/views/mapview.dart
@@ -93,60 +93,7 @@ class _MapViewState extends State<MapView> {
         ],
       ),
       body: MapDisplay(),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        elevation: 0.0,
-        currentIndex: 0,
-      
-        backgroundColor: Colors.red[800],
-        selectedItemColor: Colors.grey[50],
-        unselectedItemColor: Colors.grey[500],
-        selectedIconTheme: IconThemeData(color: Colors.white),
-        unselectedIconTheme: IconThemeData(color: Colors.grey[50]),
-
-        selectedFontSize: 0,
-        unselectedFontSize: 0,
-      
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/bn_map.png", scale: 3,),
-            label: "Map"
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/bn_camera.png", scale: 3,),
-            label: "QR Scanner"
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/bn_questionmark.png", scale: 3,),
-            label: "Helpful Links"
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset("assets/icons/bn_3dots.png", scale: 3,),
-            label: "Schedule"
-          ),
-        ],
-      
-        onTap: (index) {
-          //TODO: Abtract this into a provider?
-          switch (index) {
-            case 0:
-              Navigator.of(context).pushReplacementNamed("/map");
-              break;
-            case 1:
-              Navigator.of(context).pushReplacementNamed("/sched");
-              break;
-            case 2:
-              Navigator.of(context).pushReplacementNamed("/helpful");
-              break;
-            case 3:
-              Navigator.of(context).pushReplacementNamed("/qr");
-              break;
-          }
-        },
-      ),
+      bottomNavigationBar: CustomBottomNavBar(),
     );
   }
 }
--- lib/views/widgets/widgets_all.dart
+++ lib/views/widgets/widgets_all.dart
@@ -1,2 +1,3 @@
 export 'package:bhs__companion__app/views/widgets/mapbutton.dart';
 export 'package:bhs__companion__app/views/widgets/mapdisplay.dart';
+export 'package:bhs__companion__app/views/widgets/bottomnavbar.dart';
