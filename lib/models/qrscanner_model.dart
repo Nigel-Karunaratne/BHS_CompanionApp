2148cfa0d103308fd23539a4d8d1b390f4ed59e1
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-05-23 21:12:23 -0400

    Started work on parsing the raw data from a qr code

--- lib/models/qrscanner_model.dart
+++ lib/models/qrscanner_model.dart
@@ -5,17 +5,38 @@ import 'package:provider/provider.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 class QRScannerProvider extends ChangeNotifier {
+
+  //* NOTE: The raw text from each QR Code should be: {key} ||| Scan with the BHS Companion App to see where you are in the school!
+  //* The key NEEDS TO BE INSIDE THE CURLY BRACES. Anything after the curly braces can be different. 
   void parseBarcode(BuildContext context, Barcode barcode) {
     debugPrint(barcode.rawValue!);
 
+    bool found = false;
+    String newVal = "";
+
+    //Cycle through the raw string and extract anything in between the curly braces (inclusive) if possible
+    for(int i = 0; i < barcode.rawValue!.length; i++) {
+      newVal += barcode.rawValue!.characters.elementAt(i);
+      if(newVal[newVal.length-1] == "}" && newVal[0] == "{") {
+        found = true;
+        break;
+      }
+    }
+    //If curly braces were found, remove them and try to search for the key in the dictionary (Map). (this needs to be implemented)
+    if(found) {
+      newVal = newVal.substring(1, newVal.length-1); //This removes the curly braces
+    }
+
+
+
     //! Debug: Show a popup box that contains the data
     showDialog(context: context, builder: (BuildContext context) => AlertDialog(
-        title: const Text('Popup example'),
+        title: const Text('Read Data'),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(barcode.rawValue!),
+            Text(newVal),
           ],
         ),
         actions: <Widget>[
