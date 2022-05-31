3304680dc2a6cd12755d02e45646e95ddbbbad12
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-05-31 17:18:49 -0400

    Added 2nd floor icons + all entries into model.

--- lib/views/widgets/mapdisplay.dart
+++ lib/views/widgets/mapdisplay.dart
@@ -39,7 +39,7 @@ class _MapDisplayState extends State<MapDisplay> {
             builder: (context) {
               switch(currentFloor) {
                 case 0:
-                  return FloorBasementStack(showIcons: showMarkers);
+                  return FloorLowerStack(showIcons: showMarkers);
                 case 1:
                   return Floor1Stack(showIcons: showMarkers);
                 case 2:
@@ -113,6 +113,7 @@ class _Floor1StackState extends State<Floor1Stack> {
           "assets/img/map_floor1.png",
         ),
       ),
+
       // * Dark Green (Health)
       // MapButton(id: "154", top: 360, left: 660),
       // MapButton(id: "152", top: 360, left: 720),
@@ -167,11 +168,11 @@ class _Floor1StackState extends State<Floor1Stack> {
       // MapButton(id: "102", top: 1340, left: 740),
       // MapButton(id: "100", top: 1340, left: 910),
       // MapButton(id: "109", top: 1430, left: 665),
-      // MapButton(id: "113", top: 1510, left: 665),
+      MapButton(id: "113", top: 1510, left: 665),
 
       // * Yellow + Cyan ()
       // MapButton(id: "107", top: 1410, left: 735),
-      // MapButton(id: "113", top: 1450, left: 735),
+      MapButton(id: "113", top: 1450, left: 735),
       // MapButton(id: "101", top: 1410, left: 865),
       // MapButton(id: "103", top: 1450, left: 865),
       // MapButton(id: "105 / Bridge", top: 1430, left: 800),
@@ -220,20 +221,88 @@ class _Floor2StackState extends State<Floor2Stack> {
         ),
       ),
 
-      MapButton(id: "R1", top: 100, left: 200),
+      //* History Hall
+      MapButton(id: "401", top: 650, left: 720),
+      MapButton(id: "403", top: 650, left: 815),
+      MapButton(id: "405", top: 650, left: 900),
+      MapButton(id: "407", top: 650, left: 980),
+      MapButton(id: "409", top: 660, left: 1165),
+      MapButton(id: "411", top: 660, left: 1235),
+      MapButton(id: "413", top: 660, left: 1300),
+
+      MapButton(id: "400", top: 725, left: 655),
+      MapButton(id: "402", top: 725, left: 705),
+      MapButton(id: "404", top: 725, left: 755),
+      MapButton(id: "406", top: 725, left: 925),
+      MapButton(id: "408", top: 725, left: 985),
+      MapButton(id: "410", top: 740, left: 1045),
+      MapButton(id: "412", top: 740, left: 1255),
+      MapButton(id: "418", top: 740, left: 1320),
+
+      MapButton(id: "414", top: 815, left: 1170),
+      MapButton(id: "416", top: 815, left: 1330),
+
+      //* English Hall
+      MapButton(id: "301", top: 870, left: 200),
+      MapButton(id: "303", top: 870, left: 300),
+      MapButton(id: "305", top: 870, left: 400),
+      MapButton(id: "307", top: 870, left: 470),
+      MapButton(id: "309", top: 890, left: 705),
+      MapButton(id: "311", top: 890, left: 770),
+      MapButton(id: "313", top: 890, left: 870),
+      MapButton(id: "315", top: 890, left: 930),
+      MapButton(id: "317", top: 890, left: 995),
+
+      MapButton(id: "300", top: 960, left: 180),
+      MapButton(id: "302", top: 960, left: 245),
+      MapButton(id: "304", top: 960, left: 310),
+      MapButton(id: "306", top: 960, left: 380),
+      MapButton(id: "308", top: 960, left: 450),
+      MapButton(id: "310 / LABB", top: 960, left: 520),
+
+      MapButton(id: "314", top: 970, left: 595),
+      MapButton(id: "316", top: 970, left: 660),
+      MapButton(id: "318", top: 970, left: 750),
+      MapButton(id: "320", top: 970, left: 890),
+      MapButton(id: "322", top: 970, left: 970),
+      MapButton(id: "324", top: 970, left: 1025),
+
+      //* Math Hall
+      MapButton(id: "201", top: 1120, left: 680),
+      MapButton(id: "203", top: 1120, left: 735),
+      MapButton(id: "205", top: 1120, left: 930),
+      MapButton(id: "207", top: 1120, left: 995),
+
+      MapButton(id: "209", top: 1115, left: 1140),
+      MapButton(id: "219", top: 1050, left: 1185),
+      MapButton(id: "217", top: 1055, left: 1280),
+      MapButton(id: "215", top: 1055, left: 1350),
+      MapButton(id: "211", top: 1115, left: 1285),
+      MapButton(id: "213", top: 1115, left: 1350),
+      
+      MapButton(id: "200", top: 1200, left: 570),
+      MapButton(id: "202", top: 1200, left: 650),
+      MapButton(id: "204", top: 1200, left: 740),
+      MapButton(id: "206", top: 1200, left: 830),
+      MapButton(id: "208", top: 1200, left: 925),
+      MapButton(id: "210", top: 1200, left: 1025),
+
+      MapButton(id: "212", top: 1185, left: 1160),
+      MapButton(id: "214", top: 1190, left: 1235),
+      MapButton(id: "216", top: 1190, left: 1295),
     ];
   }
 }
 
-class FloorBasementStack extends StatefulWidget {
-  const FloorBasementStack({Key? key, required this.showIcons}) : super(key: key);
+class FloorLowerStack extends StatefulWidget {
+  const FloorLowerStack({Key? key, required this.showIcons}) : super(key: key);
   final bool showIcons;
 
   @override
-  State<FloorBasementStack> createState() => _FloorBasementStackState();
+  State<FloorLowerStack> createState() => _FloorLowerStackState();
 }
 
-class _FloorBasementStackState extends State<FloorBasementStack> {
+class _FloorLowerStackState extends State<FloorLowerStack> {
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -257,7 +326,18 @@ class _FloorBasementStackState extends State<FloorBasementStack> {
         ),
       ),
 
-      MapButton(id: "R1", top: 100, left: 200),
+      //* Dark Red
+      MapButton(id: "Wooden Gym", top: 1090, left: 370),
+      MapButton(id: "Wrestling Room", top: 1000, left: 750),
+      MapButton(id: "Rubber Gym", top: 1290, left: 820),
+      MapButton(id: "Gymnastics Room", top: 1290, left: 1100),
+
+      //* Light Blue (Locker Rooms)
+      MapButton(id: "Boy's Locker Room", top: 810, left: 890),
+      MapButton(id: "Girl's Locker Room", top: 1030, left: 1040),
+
+      //* Light Purple ()
+      MapButton(id: "Dance Room", top: 990, left: 1390),
     ];
   }
 }
