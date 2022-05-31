969359ceed9d243153bc63fdb4edd20af4f87011
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-05-31 09:08:39 -0400

    started work on the resources provider

---
+++ lib/models/resources_model.dart
@@ -0,0 +1,13 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class ResorcesProvider {
+  void openAspen() => openUrl(Uri.parse("https://ma-burlington.myfollett.com/aspen/logon.do"));
+  void openNaviance() => openUrl(Uri.parse("https://student.naviance.com/auth/fclookup"));
+  
+
+  void openUrl(Uri path) async {
+    if (!await launchUrl(path)) {
+      throw 'Could not launch $path';
+    }
+  }
+}
