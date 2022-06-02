23e5603b7af71270b7b1825373e2d86b2d433113
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-06-01 23:01:56 -0400

    added windows icon + renamed window

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"bhs_companion_app", origin, size)) {
+  if (!window.CreateAndShow(L"Explore BHS (Windows Version)", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
