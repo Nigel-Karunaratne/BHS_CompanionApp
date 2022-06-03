36b38c423eca2cbe593ee1e4506725f1a8c375e8
Nigel-Karunaratne <nigel.karunaratne36@gmail.com>
2022-06-03 11:18:27 -0400

    re-obtained packages after upgrading to 3.0.1

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\thene\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\thene\Documents\BHS Companion App\BHS_CompanionApp"
+export "FLUTTER_APPLICATION_PATH=C:\Users\thene\Documents\BHS Companion App\bhs_companion_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
